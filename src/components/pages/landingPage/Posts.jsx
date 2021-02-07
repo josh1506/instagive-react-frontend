b43784a9f55acfc15d0a1f241cd27d83ec21b8a0
josh1506 <jmjabor@gmail.com>
2021-02-07 11:14:25 +0800

    creating post container for all donation post in landing page

---
+++ src/components/pages/landingPage/Posts.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PostCard from './PostCard';
+import '../../../style/landingPage/post.css'
+
+function Posts(props) {
+    return (
+        <div className='postContainer'>
+            <h2 className='postTitle'>Posts</h2>
+            <div>
+                <div className='postFilter'>
+                    <img src="" alt="icon" />
+                    <p>Filter by:</p>
+                    <select name="" id="" ></select>
+                </div>
+                {/* post */}
+                <PostCard />
+            </div>
+        </div>
+    );
+}
+
+export default Posts;
