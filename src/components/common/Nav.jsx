ee4620f109f4ae3545be72dab60c1776070a083c
josh1506 <jmjabor@gmail.com>
2021-02-07 11:11:05 +0800

    updating and adding logo on navbar

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -5,14 +5,14 @@ import logo from '../../img/Instagive-logo-2.png'
 function Nav(props) {
     return (
         <nav class="navBar">
-            <span class='navLogo' style={{ backgroundColor: 'lightblue' }}>
-                <img src={logo} alt="logo" height='45px' />
+            <span class='navLogo'>
+                <img src={logo} alt="logo" height='45px' style={{ marginRight: 10 }} />
                 <p>InstaGive</p>
                 <span>
                     {/* Other nav links */}
                 </span>
             </span>
-            <span class='navLogin' style={{ backgroundColor: 'salmon' }}>
+            <span class='navLogin'>
                 <a href="#" className='navLink'>Signup</a>
                 /
                 <a href="#" className='navLink'>Login</a>
