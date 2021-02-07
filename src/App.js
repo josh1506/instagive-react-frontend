d10d41cb2b5098bab58913b986f1fa6f4c13cb01
josh1506 <jmjabor@gmail.com>
2021-02-07 11:18:14 +0800

    importing landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/common/Nav';
+import LandingPage from './components/pages/landingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <LandingPage />
     </div>
   );
 }
