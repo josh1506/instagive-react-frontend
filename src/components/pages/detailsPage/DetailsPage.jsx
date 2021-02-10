9ec2411579f084b0a9edfd2671cd36f2bdc04b4e
josh1506 <jmjabor@gmail.com>
2021-02-10 22:42:52 +0800

    adding other posts in details page

--- src/components/pages/detailsPage/DetailsPage.jsx
+++ src/components/pages/detailsPage/DetailsPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import OtherPosts from './../../common/OtherPosts';
+import DetailsPageContent from './DetailsPageContent';
 
 function DetailsPage(props) {
     return (
         <div>
-
+            <DetailsPageContent />
+            <OtherPosts />
         </div>
     );
 }
