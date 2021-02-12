04443187a0d4663e182174beaeae48bcac74ef8c
josh1506 <jmjabor@gmail.com>
2021-02-12 20:47:18 +0800

    removing other post in details page

--- src/components/pages/detailsPage/DetailsPage.jsx
+++ src/components/pages/detailsPage/DetailsPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import OtherPosts from './../../common/OtherPosts';
 import DetailsPageContent from './DetailsPageContent';
 
 function DetailsPage(props) {
     return (
         <div>
             <DetailsPageContent />
-            <OtherPosts />
         </div>
     );
 }
