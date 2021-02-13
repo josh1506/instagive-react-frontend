3597a383d934e81cb025dfe57541e2efcc2f334f
josh1506 <jmjabor@gmail.com>
2021-02-13 13:32:36 +0800

    creating pending page for admin

---
+++ src/components/pages/adminPage/AdminPending.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './../../common/Table';
+
+function AdminPending(props) {
+    return (
+        <div>
+            <h1>Pending</h1>
+            <Table />
+        </div>
+    );
+}
+
+export default AdminPending;
