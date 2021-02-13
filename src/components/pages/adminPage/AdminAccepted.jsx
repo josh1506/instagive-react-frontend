52f1d8750ecd37066a6ac26f6884d50fa37f22e0
josh1506 <jmjabor@gmail.com>
2021-02-13 13:31:56 +0800

    creating accepted page for admin

---
+++ src/components/pages/adminPage/AdminAccepted.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './../../common/Table';
+
+function AdminAccepted(props) {
+    return (
+        <div>
+            <h1>Accepted</h1>
+            <Table />
+        </div>
+    );
+}
+
+export default AdminAccepted;
