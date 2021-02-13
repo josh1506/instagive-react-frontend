b450fe4219240bab099559cad7810f5e0dbc5970
josh1506 <jmjabor@gmail.com>
2021-02-13 13:11:24 +0800

    creating post list for diplaying pages

---
+++ src/components/pages/userPage/PostList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostCard from './../../common/PostCard';
+
+function PostList(props) {
+    return (
+        <div>
+            <PostCard />
+        </div>
+    );
+}
+
+export default PostList;
