caf800e026ac4f925c46d82ec4a8e1375f037971
josh1506 <jmjabor@gmail.com>
2021-02-13 13:33:00 +0800

    creating rejected page for admin

---
+++ src/components/pages/adminPage/AdminRejected.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './../../common/Table';
+
+function AdminRejected(props) {
+    return (
+        <div>
+            <h1>Rejected</h1>
+            <Table />
+        </div>
+    );
+}
+
+export default AdminRejected;
