46fe0169476467e31e0e5641ef500a14fd88a3d4
josh1506 <jmjabor@gmail.com>
2021-02-13 13:09:25 +0800

    creating sidebar

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar(props) {
+    return (
+        <aside>
+            <a href="#">Create Post</a>
+            <a href="#">Post List</a>
+        </aside>
+    );
+}
+
+export default Sidebar;
