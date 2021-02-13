683c5136e1ea8c48d94e028c62b93ed5ea2debff
josh1506 <jmjabor@gmail.com>
2021-02-13 13:37:14 +0800

    adding details, auth, user, and admin page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import Nav from './components/common/Nav';
 import LandingPage from './components/pages/landingPage/LandingPage';
 import DetailsPage from './components/pages/detailsPage/DetailsPage';
+import AuthPage from './components/pages/authPage/AuthPage';
+import UserPage from './components/pages/userPage/UserPage';
+import AdminPage from './components/pages/adminPage/AdminPage';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <LandingPage />
+      {/* <LandingPage /> */}
       {/* <DetailsPage /> */}
+      {/* <AuthPage /> */}
+      {/* <UserPage /> */}
+      <AdminPage />
+
     </div>
   );
 }
