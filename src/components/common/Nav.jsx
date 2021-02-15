b3d04ae15cc6e83fb4e98da89d15e1b9a0183337
josh1506 <jmjabor@gmail.com>
2021-02-16 00:03:38 +0800

    updating navbar

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -4,17 +4,19 @@ import logo from '../../img/Instagive-logo-2.png'
 
 function Nav(props) {
     return (
-        <nav class="navBar">
-            <span class='navLogo'>
-                <img src={logo} alt="logo" height='45px' style={{ marginRight: 10 }} />
-                <p>InstaGive</p>
+        <nav className="navBar">
+            <span>
+                <a href="#" className='navLogo'>
+                    <img src={logo} alt="logo" height='45px' className='logo' style={{ marginRight: 10 }} />
+                    <p className='navLogoName'>InstaGive</p>
+                </a>
                 <span>
                     {/* Other nav links */}
                 </span>
             </span>
-            <span class='navLogin'>
+            <span className='navLogin'>
                 <a href="#" className='navLink'>Signup</a>
-                /
+                |
                 <a href="#" className='navLink'>Login</a>
             </span>
         </nav>
