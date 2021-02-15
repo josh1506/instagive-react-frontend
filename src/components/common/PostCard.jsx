fe448c238a7d4746082f1a46477763b850a64a1b
josh1506 <jmjabor@gmail.com>
2021-02-16 00:04:18 +0800

    updating postcard

--- src/components/common/PostCard.jsx
+++ src/components/common/PostCard.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import '../../style/common/postCard.css'
+
 
 function PostCard(props) {
     const [cardData, setCardData] = useState([
         {
             id: '1',
             img: {
-                src: '',
+                src: 'https://i.pinimg.com/564x/8c/7c/f4/8c7cf4bbcb6a268cac135dc569275a71.jpg',
                 alt: 'photo 1'
             },
             postTitle: 'Help David Gulpilils siblings visit',
@@ -14,7 +17,7 @@ function PostCard(props) {
         }, {
             id: '2',
             img: {
-                src: '',
+                src: 'https://i.pinimg.com/564x/8c/7c/f4/8c7cf4bbcb6a268cac135dc569275a71.jpg',
                 alt: 'photo 2'
             },
             postTitle: 'Please Help Ryoko Fight Stage 4 Cancer',
@@ -22,7 +25,7 @@ function PostCard(props) {
         }, {
             id: '3',
             img: {
-                src: '',
+                src: 'https://i.pinimg.com/564x/8c/7c/f4/8c7cf4bbcb6a268cac135dc569275a71.jpg',
                 alt: 'photo 2'
             },
             postTitle: 'Typhoon Ulysses',
@@ -30,7 +33,7 @@ function PostCard(props) {
         }, {
             id: '4',
             img: {
-                src: '',
+                src: 'https://i.pinimg.com/564x/8c/7c/f4/8c7cf4bbcb6a268cac135dc569275a71.jpg',
                 alt: 'photo 2'
             },
             postTitle: 'Help Baby Khloe',
@@ -43,11 +46,14 @@ function PostCard(props) {
         <div className='postCardContainer'>
             {cardData.map(data =>
                 <div className='postCard' key={data.id}>
-                    <img src={data.img.src} alt={data.img.alt} />
-                    <h3>{data.postTitle.substring(0, 29)}{(data.postTitle.length > 29) ? '...' : ''}</h3>
-                    <p>{(data.postDetails.length > 100) ? `${data.postDetails.substring(0, 100)}...` : data.postDetails.substring(0, 100)}</p>
+                    <div className='deleteButtonContainer'>
+                        <FontAwesomeIcon icon={faTimes} className='deleteButton' size='lg' />
+                    </div>
+                    <img src={data.img.src} alt={data.img.alt} width='100%' />
+                    <h3 style={{ paddingLeft: 5 }}>{data.postTitle.substring(0, 29)}{(data.postTitle.length > 29) ? '...' : ''}</h3>
+                    <p style={{ paddingLeft: 10, paddingRight: 10 }}>{(data.postDetails.length > 100) ? `${data.postDetails.substring(0, 100)}...` : data.postDetails.substring(0, 100)}</p>
                     <div className="buttonContainer">
-                        <button>Details</button>
+                        <button className='buttonDetails'>Details</button>
                     </div>
                 </div>
             )}
