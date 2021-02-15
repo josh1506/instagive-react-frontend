7222b615040a2f3ac40a9320cf571dc43451ca87
josh1506 <jmjabor@gmail.com>
2021-02-15 16:04:25 +0800

    updating show content

--- src/components/pages/landingPage/ShowContent.jsx
+++ src/components/pages/landingPage/ShowContent.jsx
@@ -7,7 +7,7 @@ function ShowContent(props) {
             {/* show content */}
             <div class='contentDetailsContainer'>
                 <div className="contentDetails">
-                    <h1>Welcome to InstaGive</h1>
+                    <h1 className='title'>Welcome to InstaGive</h1>
                     <div>A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.</div>
                 </div>
             </div>
