09cdec7079ef7e144e3d91f6f10ec5ad84d20195
josh1506 <jmjabor@gmail.com>
2021-02-15 09:37:33 +0800

    adding update page for post

---
+++ src/components/pages/detailsPage/DetailsUpdatePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function DetailsUpdatePage(props) {
+    return (
+        <div>
+            {props.children}
+            <div>
+                <h3>Update Title - Jan 20, 2020</h3>
+                <p>asdjasldhasjdlajsdjlaksdj</p>
+                <img src="" alt="carousel" />
+            </div>
+        </div>
+    );
+}
+
+export default DetailsUpdatePage;
