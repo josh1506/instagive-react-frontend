a6a96eb45c6872f43aec5a39136306593fd4dea1
josh1506 <jmjabor@gmail.com>
2021-02-15 16:07:49 +0800

    updating signup form

--- src/components/pages/authPage/Register.jsx
+++ src/components/pages/authPage/Register.jsx
@@ -7,17 +7,38 @@ function Register(props) {
                 <h1>Account Details</h1>
             </div>
             <form action="" className='register-form'>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username" placeholder='Username' />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" id="password" placeholder='Password' />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" id="password" placeholder='Password' />
-                <label htmlFor="password">Password</label>
-                <input type="text" name="password" id="password" placeholder='Password' />
+                <div>
+                    <img src="" alt="Upload photo here" />
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" placeholder='Email' />
+                    <label htmlFor="password">Password</label>
+                    <input type="text" name="password" id="password" placeholder='Password' />
+                    <label htmlFor="confirm-pass">Confirm Password</label>
+                    <input type="text" name="confirm-pass" id="confirm-pass" placeholder='confirm Password' />
+                </div>
+                <div>
+                    <label htmlFor="org-name">Organization Name</label>
+                    <input type="text" name="org-name" id="org-name" placeholder='Organization Name' />
+                    <label htmlFor="org-num">Organization No.</label>
+                    <input type="text" name="org-num" id="org-num" placeholder='Organization No.' />
+                    <label htmlFor="city">City</label>
+                    <input type="text" name="city" id="city" placeholder='City' />
+                    <label htmlFor="zip-code">Zip Code</label>
+                    <input type="text" name="zip-code" id="zip-code" placeholder='Zip Code' />
+                    <label htmlFor="org-address">Organization Address</label>
+                    <input type="text" name="org-address" id="org-address" placeholder='Organization Address' />
+                    <label htmlFor="org-doc">Organization Documents</label>
+                    <input type="file" id="org-doc" name="org-doc" multiple></input>
+                    <label htmlFor="rep-name">Representative Name</label>
+                    <input type="text" name="rep-name" id="rep-name" placeholder='Representative Name' />
+                    <label htmlFor="rep-id">Representative ID</label>
+                    <input type="file" id="rep-id" name="rep-id" multiple></input>
+                    <label htmlFor="org-description">Organization Description</label>
+                    <textarea name="org-description" id="org-description" cols="30" rows="5"></textarea>
+                </div>
                 <div>
                     <a href="">Cancel</a>
-                    <button>Sign up</button>
+                    <button>Send Registration</button>
                 </div>
             </form>
         </div>
