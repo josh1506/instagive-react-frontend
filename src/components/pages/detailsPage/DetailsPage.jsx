402ea80a5b36daddd0e8d7544f49024dc2f4f21c
josh1506 <jmjabor@gmail.com>
2021-02-15 09:36:04 +0800

    refactoring and adding update page for post

--- src/components/pages/detailsPage/DetailsPage.jsx
+++ src/components/pages/detailsPage/DetailsPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import DetailsPageContent from './DetailsPageContent';
+import DetailsUpdatePage from './DetailsUpdatePage';
 
 function DetailsPage(props) {
     return (
         <div>
-            <DetailsPageContent />
+            <DetailsPageContent>
+                <h1>A Big Title</h1>
+                <div className='viewUpdatesContainer'>
+                    <button>Description</button>
+                    <button>Updates</button>
+                </div>
+            </DetailsPageContent>
+            <DetailsUpdatePage>
+                <h1>A Big Title</h1>
+                <div className='viewUpdatesContainer'>
+                    <button>Description</button>
+                    <button>Updates</button>
+                </div>
+            </DetailsUpdatePage>
         </div>
     );
 }
