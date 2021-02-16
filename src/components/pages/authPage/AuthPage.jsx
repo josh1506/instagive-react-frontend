1a55a5edab57dfb799bf02c162c82254462e4405
josh1506 <jmjabor@gmail.com>
2021-02-16 15:07:23 +0800

    updating auth page

--- src/components/pages/authPage/AuthPage.jsx
+++ src/components/pages/authPage/AuthPage.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Login from './Login';
 import Register from './Register';
 import ForgotPass from './ForgotPass';
+import '../../../style/authPage/authPage.css'
+import image from '../../../img/hands-charity.png'
 
 function AuthPage(props) {
     return (
-        <div>
-            <div>
-                <img src="" alt="Image here" />
+        <div className='auth-container'>
+            <div className='authBackgroundImage'>
+                {/* <img src={image} alt="Image here" className='authImage' /> */}
             </div>
-            <div>
-                <Login />
-                <Register />
-                <ForgotPass />
+            <div className='authForm'>
+                <div className='center'>
+                    <Login />
+                    {/* <Register /> */}
+                    {/* <ForgotPass /> */}
+                </div>
             </div>
         </div>
     );
