8dc9a9f6e84de6b18a8190c7d56839165ac434cd
josh1506 <jmjabor@gmail.com>
2021-02-17 21:21:49 +0800

    adding classes and importing css for details page

--- src/components/pages/detailsPage/DetailsPage.jsx
+++ src/components/pages/detailsPage/DetailsPage.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import DetailsPageContent from './DetailsPageContent';
 import DetailsUpdatePage from './DetailsUpdatePage';
+import '../../../style/detailsPage/detailsPage.css'
 
 function DetailsPage(props) {
+
+    const renderContent = (
+        <div className='details-header-container'>
+            <h1 className='details-title'>A Big Title</h1>
+            <div className='details-button-container'>
+                <button className='details-button'>Description</button>
+                <button className='details-button'>Updates</button>
+            </div>
+        </div>
+    )
+
     return (
         <div>
-            <DetailsPageContent>
-                <h1>A Big Title</h1>
-                <div className='viewUpdatesContainer'>
-                    <button>Description</button>
-                    <button>Updates</button>
-                </div>
-            </DetailsPageContent>
-            <DetailsUpdatePage>
-                <h1>A Big Title</h1>
-                <div className='viewUpdatesContainer'>
-                    <button>Description</button>
-                    <button>Updates</button>
-                </div>
-            </DetailsUpdatePage>
+            {/* <DetailsPageContent>{renderContent}</DetailsPageContent> */}
+            {/* <DetailsUpdatePage>{renderContent}</DetailsUpdatePage> */}
         </div>
     );
 }
