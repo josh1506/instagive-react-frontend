68e447d6968694dbc188bc4caf6d4b79a5bbbbe0
josh1506 <jmjabor@gmail.com>
2021-02-20 23:08:29 +0800

    changing router from anchor tag to Link and NavLink

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import '../../style/common/Nav.css'
 import logo from '../../img/Instagive-logo-2.png'
+import { Link, NavLink } from 'react-router-dom';
 
 function Nav(props) {
     return (
         <nav className="navBar">
             <span>
-                <a href="#" className='navLogo'>
+                <Link to="/" className='navLogo'>
                     <img src={logo} alt="logo" height='45px' className='logo' style={{ marginRight: 10 }} />
                     <p className='navLogoName'>InstaGive</p>
-                </a>
+                </Link>
                 <span>
                     {/* Other nav links */}
                 </span>
             </span>
             <span className='navLogin'>
-                <a href="#" className='navLink'>Signup</a>
+                <NavLink to="/auth/register" className='navLink'>Signup</NavLink>
                 |
-                <a href="#" className='navLink'>Login</a>
+                <NavLink to="/auth/login" className='navLink'>Login</NavLink>
             </span>
         </nav>
     );
