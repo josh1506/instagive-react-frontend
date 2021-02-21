4689758ac828922250eeb8fc971efa4005a532fc
josh1506 <jmjabor@gmail.com>
2021-02-21 12:27:08 +0800

    updating variable name when getting data in post context

--- src/components/common/PostCard.jsx
+++ src/components/common/PostCard.jsx
@@ -7,19 +7,19 @@ import PostContext from './../../contex/postContext';
 
 function PostCard(props) {
     const postList = useContext(PostContext)
-
+    console.log(postList);
     return (
         <div className='postCardContainer'>
-            {postList.map(data =>
-                <div className='postCard' key={data.id}>
+            {postList.map(post =>
+                <div className='postCard' key={post.id}>
                     <div className='deleteButtonContainer'>
                         <FontAwesomeIcon icon={faTimes} className='deleteButton' size='lg' />
                     </div>
-                    <img src={data.img.src} alt={data.img.alt} width='100%' />
-                    <h3 style={{ paddingLeft: 5 }}>{data.postTitle.substring(0, 29)}{(data.postTitle.length > 29) ? '...' : ''}</h3>
-                    <p style={{ paddingLeft: 10, paddingRight: 10 }}>{(data.postDetails.length > 100) ? `${data.postDetails.substring(0, 100)}...` : data.postDetails.substring(0, 100)}</p>
+                    <img src={post.imageList[0]} alt='images' width='100%' />
+                    <h3 style={{ paddingLeft: 5 }}>{post.Title.substring(0, 29)}{(post.Title.length > 29) ? '...' : ''}</h3>
+                    <p style={{ paddingLeft: 10, paddingRight: 10 }}>{(post.description.length > 100) ? `${post.description.substring(0, 100)}...` : post.description.substring(0, 100)}</p>
                     <div className="buttonContainer">
-                        <button className='details-button' style={{ borderRadius: 7 }} onClick={() => props.history.push(`/details/${data.id}`)}>Details</button>
+                        <button className='details-button' style={{ borderRadius: 7 }} onClick={() => props.history.push(`/details/${post.id}`)}>Details</button>
                     </div>
                 </div>
             )}
