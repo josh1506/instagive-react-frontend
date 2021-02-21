7ac360c57c32465b36f48becb5cfcfea56e8725a
josh1506 <jmjabor@gmail.com>
2021-02-21 12:29:59 +0800

    updating variable name when getting data in post context

--- src/components/pages/detailsPage/DetailsPage.jsx
+++ src/components/pages/detailsPage/DetailsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import DetailsPageContent from './DetailsPageContent';
 import DetailsUpdatePage from './DetailsUpdatePage';
 import '../../../style/detailsPage/detailsPage.css'
@@ -9,9 +9,10 @@ function DetailsPage(props) {
     const postList = useContext(PostContext)
     const selectedPost = postList.filter(post => post.id === props.match.params.id)[0]
 
+    console.log(selectedPost);
     const renderContent = (
         <div className='details-header-container'>
-            <h1 className='details-title'>{selectedPost ? selectedPost.postTitle : ''}</h1>
+            <h1 className='details-title'>{selectedPost ? selectedPost.Title : ''}</h1>
             <div className='details-button-container'>
                 <button className='details-button' onClick={() => props.history.push(`/details/${selectedPost.id}`)}>Description</button>
                 <button className='details-button' onClick={() => props.history.push(`/details/${selectedPost.id}/updates`)}>Updates</button>
@@ -24,6 +25,7 @@ function DetailsPage(props) {
             <Switch>
                 <Route path={'/details/:id/updates'} render={props => <DetailsUpdatePage selectedPost={selectedPost} {...props}>{renderContent}</DetailsUpdatePage>} />
                 <Route path={'/details/:id'} render={props => <DetailsPageContent onSelectedPost={selectedPost} {...props}>{renderContent}</DetailsPageContent>} />
+                <Redirect to='/not-found' />
             </Switch>
         </div>
     );
