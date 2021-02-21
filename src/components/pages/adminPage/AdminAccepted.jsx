6bd1a8d857daa33940b8737dd6722d7150ed058e
josh1506 <jmjabor@gmail.com>
2021-02-21 19:48:56 +0800

    adding accept user page for admin

--- src/components/pages/adminPage/AdminAccepted.jsx
+++ src/components/pages/adminPage/AdminAccepted.jsx
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import Table from './../../common/Table';
+import AccountList from './../../../contex/accountList';
 
 function AdminAccepted(props) {
+    const accountList = useContext(AccountList)
+    const data = accountList.filter(account => account.accountStatus === 'pending')
+
+    const theadData = [
+        {
+            label: 'Organization',
+            name: 'orgName'
+        },
+        {
+            label: 'Email',
+            name: 'email'
+        },
+        {
+            label: 'City',
+            name: 'city'
+        },
+        {
+            label: 'Rep. Name',
+            name: 'repName'
+        },
+    ]
+
+
+    const handleEditButton = (event, account) => {
+        console.log(account);
+    }
+
+    const handleDeleteButton = (event, account) => {
+        console.log(account);
+    }
+
+
+
     return (
         <div>
-            <h1>Accepted</h1>
-            <Table />
+            <h1 className='admin-page-title'>Accepted</h1>
+            <table className='table-container'>
+                <thead>
+                    <tr>
+                        {theadData.map(tableHead =>
+                            <th key={tableHead.name} className='table-header'>{tableHead.label}</th>
+                        )}
+                        <th className='table-header'>No. of Post</th>
+                        <th className='table-header'>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(account =>
+                        <tr key={account.id}>
+                            {theadData.map(tableHead =>
+                                <td key={tableHead.name} className='table-item'>
+                                    {account[tableHead.name]}</td>
+                            )}
+                            <td className='table-item'>
+
+                            </td>
+                            <td className='table-item'>
+                                <FontAwesomeIcon
+                                    icon={faEdit}
+                                    className='table-icon'
+                                    size='lg'
+                                    onClick={e => handleEditButton(e, account)}
+                                    style={{ cursor: 'pointer' }}
+                                />
+                                <FontAwesomeIcon
+                                    icon={faTrash}
+                                    className='table-icon'
+                                    size='lg'
+                                    onClick={e => handleDeleteButton(e, account)}
+                                    style={{ cursor: 'pointer' }}
+                                />
+                            </td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     );
 }
