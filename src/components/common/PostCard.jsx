c4ffcb5c166fff24525dc6946a46624ad23e1951
josh1506 <jmjabor@gmail.com>
2021-02-22 22:41:34 +0800

    removing dummy data

--- src/components/common/PostCard.jsx
+++ src/components/common/PostCard.jsx
@@ -3,15 +3,15 @@ import React, { useState, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import '../../style/common/postCard.css';
-import PostContext from './../../contex/postContext';
+import PostContext from './../../context/postContext';
 
 function PostCard(props) {
   const postList = useContext(PostContext);
-
+  console.log(postList);
   return (
     <div className='postCardContainer'>
-      {postList.map((data) => (
-        <div className='postCard' key={data.id}>
+      {postList.map((post) => (
+        <div className='postCard' key={post.id}>
           <div className='deleteButtonContainer'>
             <FontAwesomeIcon
               icon={faTimes}
@@ -20,21 +20,21 @@ function PostCard(props) {
             />
           </div>
           <div className="category">San Fernando</div>
-          <img src={data.img.src} alt={data.img.alt} width='100%' />
+          <img src={post.imageList[0]} alt='Photo' width='100%' />
           <h3 style={{ paddingLeft: 5 }}>
-            {data.postTitle.substring(0, 29)}
-            {data.postTitle.length > 29 ? '...' : ''}
+            {post.Title.substring(0, 29)}
+            {post.Title.length > 29 ? '...' : ''}
           </h3>
           <p style={{ paddingLeft: 10, paddingRight: 10 }}>
-            {data.postDetails.length > 100
-              ? `${data.postDetails.substring(0, 100)}...`
-              : data.postDetails.substring(0, 100)}
+            {post.description.length > 100
+              ? `${post.description.substring(0, 100)}...`
+              : post.description.substring(0, 100)}
           </p>
           <div className='buttonContainer'>
             <button
               className='details-button'
               style={{ borderRadius: 7 }}
-              onClick={() => props.history.push(`/details/${data.id}`)}
+              onClick={() => props.history.push(`/details/${post.id}`)}
             >
               Details
             </button>
