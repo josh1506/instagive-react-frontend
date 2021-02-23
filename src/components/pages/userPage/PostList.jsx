b19020b6daacea5bcdf19f3f9aa97f79877e7fa3
josh1506 <jmjabor@gmail.com>
2021-02-23 23:08:34 +0800

    adding plus icon for adding new post

--- src/components/pages/userPage/PostList.jsx
+++ src/components/pages/userPage/PostList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import PostCard from './../../common/PostCard';
 
 function PostList(props) {
     return (
         <div>
+            <div className='user-items-right-container'>
+                <FontAwesomeIcon
+                    icon={faPlusCircle}
+                    className='deleteButton'
+                    size='lg'
+                    onClick={() => props.history.push('/user/post-create')}
+                />
+            </div>
             <PostCard />
         </div>
     );
