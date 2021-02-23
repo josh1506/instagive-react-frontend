972e3b8e3027cddb8550143aa23a8341b72ac7bc
josh1506 <jmjabor@gmail.com>
2021-02-23 23:09:46 +0800

    adding route for create, details and list of post

--- src/components/pages/userPage/UserPage.jsx
+++ src/components/pages/userPage/UserPage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom'
 import PostList from './PostList';
 import PostCreate from './PostCreate';
+import UserPostDetails from './UserPostDetails';
 
 function UserPage(props) {
     return (
         <div>
-            <PostList />
-            <PostCreate />
+            <div>
+                <Switch>
+                    <Route path='/user/post-create' component={PostCreate} />
+                    <Route path='/user/post-details' component={UserPostDetails} />
+                    <Route path='/user/' exact component={PostList} />
+                </Switch>
+            </div>
         </div>
     );
 }
