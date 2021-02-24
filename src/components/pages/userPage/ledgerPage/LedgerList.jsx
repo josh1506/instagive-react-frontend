c5486ba2ad35563aaa61da67a8884b4d4c5c6bf9
josh1506 <jmjabor@gmail.com>
2021-02-24 22:55:12 +0800

    adding leader page for displaying all user donation record

---
+++ src/components/pages/userPage/ledgerPage/LedgerList.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function LedgerList(props) {
+    return (
+        <div>
+            <div>
+                <input type="text" />
+            </div>
+            <div>
+                <button>Icon Plus</button>
+            </div>
+            <div style={{ overflowX: 'auto' }}>
+                <table>
+                    <tr>
+                        <th>Post</th>
+                        <th>Name</th>
+                        <th>Email(optional)</th>
+                        <th>Donation Type</th>
+                        <th>Payment Method(if money donation)</th>
+                        <th>Amoung/Item</th>
+                        <th>Remarks</th>
+                        <th>Date</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>Jill</td>
+                        <td>Smith</td>
+                        <td>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>Eve</td>
+                        <td>Jackson</td>
+                        <td>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Adam</td>
+                        <td>Johnson</td>
+                        <td>Adam</td>
+                        <td>Johnson</td>
+                        <td>Adam</td>
+                        <td>Johnson</td>
+                        <td>Adam</td>
+                        <td>Johnson</td>
+                        <td>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default LedgerList;
