fc1f025e8eb9139a0f6975c6a7001288facbe1d1
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-02-25 23:17:14 +0800

    adding ledger context

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,16 @@ import AuthPage from './components/pages/authPage/AuthPage';
 import UserPage from './components/pages/userPage/UserPage';
 import AdminPage from './components/pages/adminPage/AdminPage';
 import PostContext from './context/postContext';
-import './App.css';
 import NotFound from './components/common/NotFound';
 import AccountList from './context/accountList';
+import UserLedger from './context/userLedger';
+import './App.css';
 
 const axios = require('axios');
 
 function App() {
   const [post, setPost] = useState([])
+  const [userLedger, setUserLedger] = useState([])
   const [accountList, setAccountList] = useState({
     accepted: [],
     pending: [],
@@ -26,31 +28,39 @@ function App() {
     // axios post
     // axios list
     setPost([]);
-    const userList = async () => {
+    const getAllData = async () => {
       await axios
         .get('http://localhost:5000/admin/getusers')
         .then((data) => setAccountList(data.data));
-        await axios.get('http://localhost:5000/landing').then((data) => console.log(data.data))
+
+      await axios.get('http://localhost:5000/landing')
+        .then((data) => console.log(data.data))
+
+      // got ledger post here
+      // await axios.get('')
+      //   .then((data) => setUserLedger(data.data));
     };
-    
-    userList();
+
+    getAllData();
   }, []);
 
   return (
     <PostContext.Provider value={post}>
       <AccountList.Provider value={accountList}>
-        <div className='App'>
-          <Nav />
-          <Switch>
-            <Route path='/details/:id' component={DetailsPage} />
-            <Route path='/auth' component={AuthPage} />
-            <Route path='/user' component={UserPage} />
-            <Route path='/not-found' component={NotFound} />
-            <Route path='/admin' component={AdminPage} />
-            <Route path='/' exact component={LandingPage} />
-            <Redirect to='/not-found' />
-          </Switch>
-        </div>
+        <UserLedger.Provider value={userLedger}>
+          <div className='App'>
+            <Nav />
+            <Switch>
+              <Route path='/details/:id' component={DetailsPage} />
+              <Route path='/auth' component={AuthPage} />
+              <Route path='/user' component={UserPage} />
+              <Route path='/not-found' component={NotFound} />
+              <Route path='/admin' component={AdminPage} />
+              <Route path='/' exact component={LandingPage} />
+              <Redirect to='/not-found' />
+            </Switch>
+          </div>
+        </UserLedger.Provider>
       </AccountList.Provider>
     </PostContext.Provider>
   );
