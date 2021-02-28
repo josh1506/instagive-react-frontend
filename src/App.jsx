ab3fadf2563beaf6dfa282532cd9b50afff27a63
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-02-28 19:33:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,19 @@ import AdminPage from './components/pages/adminPage/AdminPage';
 import PostContext from './context/postContext';
 import NotFound from './components/common/NotFound';
 import AccountList from './context/accountList';
-import UserLedger from './context/userLedger';
+import UserContext from './context/userContext';
+import AuthContext from './context/authContext';
 import './App.css';
 
 const axios = require('axios');
 
 function App() {
+  const [auth, setAuth] = useState({ token: '', type: '' })
   const [post, setPost] = useState([])
-  const [userLedger, setUserLedger] = useState([])
+  const [user, setUser] = useState({
+    ledger: [],
+    posts: []
+  })
   const [accountList, setAccountList] = useState({
     accepted: [],
     pending: [],
@@ -24,43 +29,64 @@ function App() {
   })
 
   useEffect(() => {
-    // Run API here
-    // axios post
-    // axios list
-    setPost([]);
     const getAllData = async () => {
+      // Getting all user list
       await axios
         .get('http://localhost:5000/admin/getusers')
         .then((data) => setAccountList(data.data));
 
+      // Getting all data for landing page
       await axios.get('http://localhost:5000/landing')
         .then((data) => console.log(data.data))
 
-      // got ledger post here
-      // await axios.get('')
-      //   .then((data) => setUserLedger(data.data));
+      // Getting all data for ledger
+      await axios.get('')
+        .then(({ data }) => setUser({ ...user, ledger: data }));
+
+      // Getting all data for post
+      await axios.get('')
+        .then(({ data }) => setUser({ ...user, post: data }));
     };
 
+    const validateAuthID = async () => {
+      // Validating if token is valid
+      await axios.post('/checkusertoken', token)
+        .catch(({ data }) => {
+          // if (!data.valid) {
+          //   localStorage.removeItem(auth.type)
+          // }
+        })
+    }
+
+    // check if there's a user in local storage
+    const token = localStorage.getItem('user')
+    const type = localStorage.key(token)
+    setAuth({ token, type })
+
+    validateAuthID()
     getAllData();
   }, []);
+
 
   return (
     <PostContext.Provider value={post}>
       <AccountList.Provider value={accountList}>
-        <UserLedger.Provider value={userLedger}>
-          <div className='App'>
-            <Nav />
-            <Switch>
-              <Route path='/details/:id' component={DetailsPage} />
-              <Route path='/auth' component={AuthPage} />
-              <Route path='/user' component={UserPage} />
-              <Route path='/not-found' component={NotFound} />
-              <Route path='/admin' component={AdminPage} />
-              <Route path='/' exact component={LandingPage} />
-              <Redirect to='/not-found' />
-            </Switch>
-          </div>
-        </UserLedger.Provider>
+        <UserContext.Provider value={user}>
+          <AuthContext.Provider value={auth}>
+            <div className='App'>
+              <Nav />
+              <Switch>
+                <Route path='/details/:id' component={DetailsPage} />
+                <Route path='/auth' component={AuthPage} />
+                <Route path='/user' component={UserPage} />
+                <Route path='/not-found' component={NotFound} />
+                <Route path='/admin' component={AdminPage} />
+                <Route path='/' exact component={LandingPage} />
+                <Redirect to='/not-found' />
+              </Switch>
+            </div>
+          </AuthContext.Provider>
+        </UserContext.Provider>
       </AccountList.Provider>
     </PostContext.Provider>
   );
