27984b93d333a5c1740f2bf591fb0b29875b06b4
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-02-28 19:31:32 +0800

    adding change pass route and moving token validation in app.js

--- src/components/pages/userPage/UserPage.jsx
+++ src/components/pages/userPage/UserPage.jsx
@@ -1,39 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 import PostList from './PostList';
 import LedgerPage from './ledgerPage/LedgerPage';
 import PostCreate from './postDetailPage/PostCreate';
 import UserPostDetails from './postDetailPage/UserPostDetails';
-import axios from 'axios'
+import AuthContext from './../../../context/authContext';
+import ChangePass from './changePassPage/ChangePass';
 
 function UserPage(props) {
+    const { token } = useContext(AuthContext)
+    const [userToken, setUserToken] = useState()
+
     useEffect(() => {
-        // check if there's a user in local storage
-        const userToken = { token: localStorage.getItem('user') }
-
-        const validateAuthID = async () => {
-            // Same logic for user login
-            // axios for validating ID
-            await axios.post('/checkusertoken', userToken)
-                .then(({ data }) => {
-                    // data.repName
-                })
-                .catch(({ data }) => {
-                    // if data.valid{
-                    // localStorage.removeItem('user')
-                    // props.history.push('/auth')
-                    // }
-                })
-        }
-
-        validateAuthID()
-        if (!userToken) props.history.replace('/auth')
+        setUserToken(token)
+        if (!localStorage.getItem('user')) return props.history.push('/auth')
     }, [])
 
     return (
         <div>
             <div>
                 <Switch>
+                    <Route path='/user/change-password' component={ChangePass} />
                     <Route path='/user/post-create' component={PostCreate} />
                     <Route path='/user/post-details' component={UserPostDetails} />
                     <Route path='/user/ledger' component={LedgerPage} />
