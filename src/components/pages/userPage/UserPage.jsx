70c8df86fbf4a2e8fe194666db1f884db8b3f21c
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-03-04 21:02:08 +0800

    removed all api request and change the context to redux

--- src/components/pages/userPage/UserPage.jsx
+++ src/components/pages/userPage/UserPage.jsx
@@ -1,76 +1,44 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import PostList from './PostList';
 import LedgerPage from './ledgerPage/LedgerPage';
 import PostCreate from './postDetailPage/PostCreate';
 import UserPostDetails from './postDetailPage/UserPostDetails';
-import AuthContext from './../../../context/authContext';
 import ChangePass from './changePassPage/ChangePass';
-import UserContext from './../../../context/userContext';
-import axios from 'axios'
 import UpdateUserPost from './postUpdatePage/UpdateUserPost';
+import { authRemove } from '../../../app/auth'
+import { userLedgerFetch, userPostFetch } from '../../../app/user'
 
 function UserPage(props) {
-    const { token } = useContext(AuthContext)
-    const [userToken, setUserToken] = useState()
-    const [user, setUser] = useState({
-        ledger: [],
-        post: []
-    })
-
     useEffect(() => {
-        setUserToken(token)
         if (!localStorage.getItem('user')) return props.history.push('/auth')
+        props.userLedgerFetch(props.auth.token)
+        props.userPostFetch(props.auth.token)
 
-        const getUserData = async () => {
-            // Getting all data for ledger
-            // await axios.get(`/getledger/${userToken}`)
-            //     .then(({ data }) => setUser({ ...user, ledger: data }));
+    }, [])
 
-            // Getting all data for post
-           
-
-           const {data:ledger} = await axios.post('http://localhost:5000/ledger/getall' ,{token: localStorage.getItem('user')})
-
-
-
-
-
-           const {data:post} = await axios.post('http://localhost:5000/post/userpost', {token: localStorage.getItem('user')})
-            
-            
-                setUser({
-                    ledger,
-                    post
-
-                })
-
-            }
-
-
-
-
-            getUserData()
-            
-        }, [])
-        
     return (
         <div>
-            <UserContext.Provider value={user}>
-                <div>
-                    <Switch>
-                        <Route path='/user/change-password' component={ChangePass} />
-                        <Route path='/user/post-create' component={PostCreate} />
-                        <Route path='/user/post-details/:id' component={UserPostDetails} />
-                        <Route path='/user/ledger' component={LedgerPage} />
-                        <Route path='/user/update-details/:id' component={UpdateUserPost} />
-                        <Route path='/user/' exact component={PostList} />
-                        <Redirect to='/not-found' />
-                    </Switch>
-                </div>
-            </UserContext.Provider>
+            <div>
+                <Switch>
+                    <Route path='/user/change-password' component={ChangePass} />
+                    <Route path='/user/post-create' component={PostCreate} />
+                    <Route path='/user/post-details/:id' component={UserPostDetails} />
+                    <Route path='/user/ledger' component={LedgerPage} />
+                    <Route path='/user/update-details/:id' component={UpdateUserPost} />
+                    <Route path='/user/' exact component={PostList} />
+                    <Redirect to='/not-found' />
+                </Switch>
+            </div>
         </div>
     );
 }
 
-export default UserPage;
+
+const mapStateToProps = (state) => {
+    return state
+}
+
+export default connect(mapStateToProps, { authRemove, userLedgerFetch, userPostFetch })(UserPage);
