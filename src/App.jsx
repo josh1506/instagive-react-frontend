82aa81f2770459419163cc12c188780a485f6da7
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-03-04 20:53:45 +0800

    removing axios and changing context to redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux'
+
+import { postListFeched } from './app/postList'
+import { accountFetched } from './app/accounts'
+import { authAdd } from './app/auth'
 import Nav from './components/common/Nav';
 import LandingPage from './components/pages/landingPage/LandingPage';
 import DetailsPage from './components/pages/detailsPage/DetailsPage';
 import AuthPage from './components/pages/authPage/AuthPage';
 import UserPage from './components/pages/userPage/UserPage';
 import AdminPage from './components/pages/adminPage/AdminPage';
-import PostContext from './context/postContext';
 import NotFound from './components/common/NotFound';
-import AccountList from './context/accountList';
-import AuthContext from './context/authContext';
 import './App.css';
 
-const axios = require('axios');
 
-function App() {
-  const [auth, setAuth] = useState({ token: '', type: '' })
-  const [post, setPost] = useState([])
-  const [accountList, setAccountList] = useState({
-    accepted: [],
-    pending: [],
-    rejected: []
-  })
+function App(props) {
 
   useEffect(() => {
     const getAllData = async () => {
-      // Getting all user list
-      await axios
-        .get('http://localhost:5000/admin/getusers')
-        .then((data) => setAccountList(data.data));
-
-      // Getting all data for landing page
-      await axios.get('http://localhost:5000/landing')
-        .then(({ data }) => setPost(data))
+      props.accountFetched()
+      props.postListFeched()
     };
 
     const validateAuthID = async () => {
       // Validating if token is valid
-      await axios.post('/checkusertoken', token)
-        .catch(({ data }) => {
-          // if (!data.valid) {
-          //   localStorage.removeItem(auth.type)
-          // }
-        })
+      // await axios.post('/checkusertoken', token)
+
+      props.authAdd(token, type)
     }
 
     // check if there's a user in local storage
     const token = localStorage.getItem('user')
     const type = localStorage.key(token)
-    setAuth({ token, type })
 
     validateAuthID()
     getAllData();
@@ -56,25 +40,25 @@ function App() {
 
 
   return (
-    <PostContext.Provider value={post}>
-      <AccountList.Provider value={accountList}>
-        <AuthContext.Provider value={auth}>
-          <div className='App'>
-            <Nav />
-            <Switch>
-              <Route path='/details/:id' component={DetailsPage} />
-              <Route path='/auth' component={AuthPage} />
-              <Route path='/user' component={UserPage} />
-              <Route path='/not-found' component={NotFound} />
-              <Route path='/admin' component={AdminPage} />
-              <Route path='/' exact component={LandingPage} />
-              <Redirect to='/not-found' />
-            </Switch>
-          </div>
-        </AuthContext.Provider>
-      </AccountList.Provider>
-    </PostContext.Provider>
+    <div className='App'>
+      <Nav />
+      <Switch>
+        <Route path='/details/:id' component={DetailsPage} />
+        <Route path='/auth' component={AuthPage} />
+        <Route path='/user' component={UserPage} />
+        <Route path='/not-found' component={NotFound} />
+        <Route path='/admin' component={AdminPage} />
+        <Route path='/' exact component={LandingPage} />
+        <Redirect to='/not-found' />
+      </Switch>
+    </div>
   );
 }
 
-export default App;
+
+const mapStateToProps = state => {
+  return { state }
+}
+
+export default connect(mapStateToProps,
+  { authAdd, postListFeched, accountFetched })(App);
