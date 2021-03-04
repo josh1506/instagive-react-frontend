47ef9a8b1499e5f24e28484ee2737641aef08ac3
Joshua Michael Jabor <51785959+josh1506@users.noreply.github.com>
2021-03-04 20:54:26 +0800

    removing all context file and changing them to redux

--- src/context/accountList.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const AccountList = React.createContext()
-AccountList.displayName = 'account list'
-
-export default AccountList
--- src/context/authContext.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const AuthContext = React.createContext()
-AuthContext.displayName = 'auth'
-
-export default AuthContext
