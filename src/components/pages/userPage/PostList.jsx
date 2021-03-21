0b6bbc1204fd303b762a4184e1b068eaac00964a
aaronjoshua27 <73965654+aaronpangan@users.noreply.github.com>
2021-03-21 22:16:41 +0800

    reject post user

--- src/components/pages/userPage/PostList.jsx
+++ src/components/pages/userPage/PostList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -20,30 +20,17 @@ import {
   DialogContentText,
   DialogTitle,
 } from '@material-ui/core/';
-
-
-
-
-
+import axios from 'axios';
 
 function PostList(props) {
-
-
-  const [openModal, setModal] = useState(false);
-  const handleClickOpen = async () => {
-    setModal(true);
+  const [openModal, setModal] = useState({ value: '', postId: '' });
+  const handleClickOpen = async (postId) => {
+    setModal({ value: true, postId });
   };
 
   const handleClose = () => {
-    setModal(false);
+    setModal({ value: false, postId: '' });
   };
-
-
-
-
-
-
-
 
   const useStyles = makeStyles({
     root: {
@@ -65,280 +52,273 @@ function PostList(props) {
       </div>
     );
 
+  const handleReject = async () => {
+    await axios.post(
+      `http://localhost:5000/post/poststatus/${openModal.postId}/Rejected`,
+      { token: localStorage.getItem('user') }
+    );
 
-      const handleDelete = async () => {
+    handleClose();
 
-          console.log('Meow')
-
-      }
-
-
-
+    document.location.reload();
+  };
 
   return (
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
     <div>
-
-
-
-<Dialog
-        open={openModal}
+      <Dialog
+        open={openModal.value}
         onClose={handleClose}
         aria-labelledby='form-dialog-title'
         maxWidth='sm'
         fullWidth={true}
       >
-
-
         <DialogContent>
-          <DialogContentText>Are you sure you want to delete this post?</DialogContentText>
+          <DialogContentText>
+            Are you sure you want to delete this post?
+          </DialogContentText>
+        </DialogContent>
 
-    </DialogContent>
-
-    <DialogActions>
+        <DialogActions>
           <Button onClick={handleClose} color='primary'>
             Close
           </Button>
 
-          <Button onClick={handleDelete} color='secondary'>
+          <Button onClick={handleReject} color='secondary'>
             Delete
           </Button>
-
-
-
         </DialogActions>
-</Dialog>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      </Dialog>
 
       <Button
-          style={{margin: '12px'}}
-
-onClick={() => props.history.push('/user/post-create')}
+        style={{ margin: '12px' }}
+        onClick={() => props.history.push('/user/post-create')}
         variant='outlined'
         color='primary'
-        endIcon={<AddIcon fontSize="small"></AddIcon>
-    }
-      >Create Post</Button>
-                                  <h3 style={{textAlign:'center'}}>Approved Post</h3>
+        endIcon={<AddIcon fontSize='small'></AddIcon>}
+      >
+        Create Post
+      </Button>
+      <h3 style={{ textAlign: 'center' }}>Approved Post</h3>
 
       <div className='postCardContainer shadow-container'>
+        {props.post.filter((post) => post.status === 'Approved').length ===
+        0 ? (
+          <h2>You have no approved post</h2>
+        ) : (
+          props.post.map((post) =>
+            post.status === 'Approved' ? (
+              <div className='hover-container'>
+                <Card
+                  key={post._id}
+                  className={classes.root}
+                  style={{ margin: '20px' }}
+                >
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      alt={post.profilePic}
+                      height='140'
+                      image={`/docs/${post.profilePic}`}
+                      title={post.profilePic}
+                    />
+                    <CardContent>
+                      <Typography
+                        color='primary'
+                        gutterBottom
+                        variant='h5'
+                        component='h2'
+                      >
+                        {post.Title.substring(0, 20)}
+                        {post.Title.length > 20 ? '...' : ''}
+                      </Typography>
+                      <Typography className={classes.pos} color='textSecondary'>
+                        {post.location} |{' '}
+                        {post.donationType === 'both'
+                          ? 'Cash, In-kind'
+                          : post.donationType === 'cash'
+                          ? 'Cash'
+                          : 'In-kind'}
+                      </Typography>
+                      <Typography
+                        style={{ height: '110px' }}
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                      >
+                        {post.description.substring(0, 255)}
+                        {post.description.length > 250 ? '...' : ''}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size='small'
+                      color='primary'
+                      onClick={() =>
+                        props.history.push(`/user/post-details/${post._id}`)
+                      }
+                    >
+                      View Post
+                    </Button>
 
-         {props.post.filter(post => post.status === 'Approved').length === 0 ? <h2>You have no approved post</h2> : props.post.map((post)  => post.status === 'Approved' ?  (
-
-
-          <div className="hover-container">
-
-
-            <Card className={classes.root} style={{ margin: '20px' }}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                alt={post.profilePic}
-                height='140'
-                image={`/docs/${post.profilePic}`}
-                title={post.profilePic}
-              />
-              <CardContent>
-                <Typography color="primary" gutterBottom variant='h5' component='h2'>
-                  {post.Title.substring(0, 20)}
-                  {post.Title.length > 20 ? '...' : ''}
-                </Typography>
-                <Typography className={classes.pos} color='textSecondary'>
-                  {post.location} |{' '}
-                  {post.donationType === 'both'
-                    ? 'Cash, In-kind'
-                    : post.donationType === 'cash'
-                    ? 'Cash'
-                    : 'In-kind'}
-                </Typography>
-                <Typography style={{height: '110px'}} variant='body2' color='textSecondary' component='p'>
-                  {post.description.substring(0, 255)}
-                  {post.description.length > 250 ? '...' : ''}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <Button
-                size='small'
-                color='primary'
-                onClick={() =>
-                  props.history.push(`/user/post-details/${post._id}`)
-                }
-              >
-               View Post
-              </Button>
-              
-              <Button
-                size='small'
-                color='secondary'
-                onClick={handleClickOpen}
-              >
-               Delete Post
-              </Button>
-          
-          
-          
-                
-                
-          
-            </CardActions>
-          </Card>
-       
-       
-       
-          </div>
-       
-       ) : '' )}
+                    <Button
+                      size='small'
+                      color='secondary'
+                      onClick={() => handleClickOpen(post._id)}
+                    >
+                      Delete Post
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            ) : (
+              ''
+            )
+          )
+        )}
       </div>
-      <h3 style={{textAlign:'center'}}>Pending Post</h3>
+      <h3 style={{ textAlign: 'center' }}>Pending Post</h3>
 
       <div className='postCardContainer shadow-container'>
+        {props.post.filter((post) => post.status === 'Pending').length === 0 ? (
+          <h2>You have no pending post</h2>
+        ) : (
+          props.post.map((post) =>
+            post.status === 'Pending' ? (
+              <div className='hover-container'>
+                <Card className={classes.root} style={{ margin: '20px' }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      alt={post.profilePic}
+                      height='140'
+                      image={`/docs/${post.profilePic}`}
+                      title={post.profilePic}
+                    />
+                    <CardContent>
+                      <Typography
+                        color='primary'
+                        gutterBottom
+                        variant='h5'
+                        component='h2'
+                      >
+                        {post.Title.substring(0, 20)}
+                        {post.Title.length > 20 ? '...' : ''}
+                      </Typography>
+                      <Typography className={classes.pos} color='textSecondary'>
+                        {post.location} |{' '}
+                        {post.donationType === 'both'
+                          ? 'Cash, In-kind'
+                          : post.donationType === 'cash'
+                          ? 'Cash'
+                          : 'In-kind'}
+                      </Typography>
+                      <Typography
+                        style={{ height: '110px' }}
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                      >
+                        {post.description.substring(0, 255)}
+                        {post.description.length > 250 ? '...' : ''}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size='small'
+                      color='primary'
+                      onClick={() =>
+                        props.history.push(`/user/post-details/${post._id}`)
+                      }
+                    >
+                      View Post
+                    </Button>
 
-{props.post.filter(post => post.status === 'Pending').length === 0  ? <h2>You have no pending post</h2> : props.post.map((post)  => post.status === 'Pending' ?  (
+                    <Button
+                      size='small'
+                      color='secondary'
+                      onClick={() => handleClickOpen(post._id)}
+                    >
+                      Delete Post
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            ) : (
+              ''
+            )
+          )
+        )}
+      </div>
 
+      <h3 style={{ textAlign: 'center' }}>Deleted/Rejected Post</h3>
 
- <div className="hover-container">
-
-   <Card className={classes.root} style={{ margin: '20px' }}>
-   <CardActionArea>
-     <CardMedia
-       component='img'
-       alt={post.profilePic}
-       height='140'
-       image={`/docs/${post.profilePic}`}
-       title={post.profilePic}
-     />
-     <CardContent>
-       <Typography color="primary" gutterBottom variant='h5' component='h2'>
-         {post.Title.substring(0, 20)}
-         {post.Title.length > 20 ? '...' : ''}
-       </Typography>
-       <Typography className={classes.pos} color='textSecondary'>
-         {post.location} |{' '}
-         {post.donationType === 'both'
-           ? 'Cash, In-kind'
-           : post.donationType === 'cash'
-           ? 'Cash'
-           : 'In-kind'}
-       </Typography>
-       <Typography style={{height: '110px'}} variant='body2' color='textSecondary' component='p'>
-         {post.description.substring(0, 255)}
-         {post.description.length > 250 ? '...' : ''}
-       </Typography>
-     </CardContent>
-   </CardActionArea>
-   <CardActions>
-     <Button
-       size='small'
-       color='primary'
-       onClick={() =>
-         props.history.push(`/user/post-details/${post._id}`)
-       }
-     >
-      View Post
-     </Button>
-   </CardActions>
- </Card>
-
-
-
- </div>
-
-): '')}
-</div>
-
-
-<h3 style={{textAlign:'center'}}>Deleted/Rejected Post</h3>
-
-
-<div className='postCardContainer shadow-container'>
-
-{props.post.filter(post => post.status === 'Rejected').length === 0 ? <h2>You have no rejected/deleted post</h2> : props.post.map((post)  => post.status === 'Rejected' ?  (
-
-
- <div className="hover-container">
-
-
-   <Card className={classes.root} style={{ margin: '20px' }}>
-   <CardActionArea>
-     <CardMedia
-       component='img'
-       alt={post.profilePic}
-       height='140'
-       image={`/docs/${post.profilePic}`}
-       title={post.profilePic}
-     />
-     <CardContent>
-       <Typography color="primary" gutterBottom variant='h5' component='h2'>
-         {post.Title.substring(0, 20)}
-         {post.Title.length > 20 ? '...' : ''}
-       </Typography>
-       <Typography className={classes.pos} color='textSecondary'>
-         {post.location} |{' '}
-         {post.donationType === 'both'
-           ? 'Cash, In-kind'
-           : post.donationType === 'cash'
-           ? 'Cash'
-           : 'In-kind'}
-       </Typography>
-       <Typography style={{height: '110px'}} variant='body2' color='textSecondary' component='p'>
-         {post.description.substring(0, 255)}
-         {post.description.length > 250 ? '...' : ''}
-       </Typography>
-     </CardContent>
-   </CardActionArea>
-   <CardActions>
-     <Button
-       size='small'
-       color='primary'
-       onClick={() =>
-         props.history.push(`/user/post-details/${post._id}`)
-       }
-     >
-      View Post
-     </Button>
-   </CardActions>
- </Card>
-
-
-
- </div>
-
-): '')}
-</div>
-
-
-
-
-
+      <div className='postCardContainer shadow-container'>
+        {props.post.filter((post) => post.status === 'Rejected').length ===
+        0 ? (
+          <h2>You have no rejected/deleted post</h2>
+        ) : (
+          props.post.map((post) =>
+            post.status === 'Rejected' ? (
+              <div className='hover-container'>
+                <Card className={classes.root} style={{ margin: '20px' }}>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      alt={post.profilePic}
+                      height='140'
+                      image={`/docs/${post.profilePic}`}
+                      title={post.profilePic}
+                    />
+                    <CardContent>
+                      <Typography
+                        color='primary'
+                        gutterBottom
+                        variant='h5'
+                        component='h2'
+                      >
+                        {post.Title.substring(0, 20)}
+                        {post.Title.length > 20 ? '...' : ''}
+                      </Typography>
+                      <Typography className={classes.pos} color='textSecondary'>
+                        {post.location} |{' '}
+                        {post.donationType === 'both'
+                          ? 'Cash, In-kind'
+                          : post.donationType === 'cash'
+                          ? 'Cash'
+                          : 'In-kind'}
+                      </Typography>
+                      <Typography
+                        style={{ height: '110px' }}
+                        variant='body2'
+                        color='textSecondary'
+                        component='p'
+                      >
+                        {post.description.substring(0, 255)}
+                        {post.description.length > 250 ? '...' : ''}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button
+                      size='small'
+                      color='primary'
+                      onClick={() =>
+                        props.history.push(`/user/post-details/${post._id}`)
+                      }
+                    >
+                      View Post
+                    </Button>
+                  </CardActions>
+                </Card>
+              </div>
+            ) : (
+              ''
+            )
+          )
+        )}
+      </div>
     </div>
   );
 }
