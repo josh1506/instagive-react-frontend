8a861d41c4dc0a4ef0716e6102293c667627af6c
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-05-06 18:01:07 +0800

    Update Nav.jsx

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { connect } from 'react-redux'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-
-import { authRemove } from '../../app/auth'
-import logo from '../../img/Instagive-logo-2.png'
-import '../../style/common/Nav.css'
+import { authRemove } from "../../app/auth";
+import logo from "../../img/Instagive-logo-2.png";
+import "../../style/common/Nav.css";
 
 function Nav(props) {
     const [anchorEl, setAnchorEl] = React.useState(null);
@@ -27,21 +26,41 @@ function Nav(props) {
 
     return (
         <nav className="navBar">
-            <span style={{display: 'flex'}}>
-                <Link to="/admin" className='navLogo'>
-                    <img src={logo} alt="logo" height='45px' className='logo' style={{ marginRight: 10 }} />
+            <span style={{ display: "flex" }}>
+                <Link to="/admin" className="navLogo">
+                    <img
+                        src={logo}
+                        alt="logo"
+                        height="45px"
+                        className="logo"
+                        style={{ marginRight: 10 }}
+                    />
                 </Link>
-                    <Link to='/' style={{textDecoration: 'none', display: 'flex', alignItems: 'center'}}>
-                        <p className='navLogoName' style={{fontSize: 22, color: '#fff'}}>InstaGive</p>
-                    </Link>
-                <span>
-                    {/* Other nav links */}
-                </span>
+                <Link
+                    to="/"
+                    style={{
+                        textDecoration: "none",
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <p
+                        className="navLogoName"
+                        style={{ fontSize: 22, color: "#fff" }}
+                    >
+                        InstaGive
+                    </p>
+                </Link>
+                <span></span>
             </span>
-            <span className='navLogin'>
-                {props.auth.token ?
+            <span className="navLogin">
+                {props.auth.type === "admin" && (
+                    <p style={{ marginRight: 20 }}>{props.auth.type}</p>
+                )}
+                {props.auth.token ? (
                     <span>
-                        {props.auth.type === 'user' || localStorage.getItem('user') ?
+                        {props.auth.type === "user" ||
+                        localStorage.getItem("user") ? (
                             <span>
                                 <div>
                                     <IconButton
@@ -51,63 +70,109 @@ function Nav(props) {
                                         onClick={handleMenu}
                                         color="inherit"
                                     >
-                                        <AccountCircle fontSize='large' />
+                                        <AccountCircle fontSize="large" />
                                     </IconButton>
                                     <Menu
                                         id="menu-appbar"
                                         anchorEl={anchorEl}
                                         anchorOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
+                                            vertical: "top",
+                                            horizontal: "right",
                                         }}
                                         keepMounted
                                         transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
+                                            vertical: "top",
+                                            horizontal: "right",
                                         }}
                                         open={open}
                                         onClose={handleClose}
                                     >
+                                        <div
+                                            style={{
+                                                padding: "5px 10px",
+                                                borderBottom:
+                                                    "1px solid lightgray",
+                                            }}
+                                        >
+                                            Logged in as{" "}
+                                            {localStorage.getItem("name")}:
+                                        </div>
                                         <MenuItem>
-                                            <NavLink to="/user/ledger" className='navLink'>Ledger</NavLink>
+                                            <NavLink
+                                                to="/user/ledger"
+                                                className="navLink"
+                                            >
+                                                Ledger
+                                            </NavLink>
                                         </MenuItem>
                                         <MenuItem>
-                                            <NavLink to="/user" className='navLink'>Dashboard</NavLink>
+                                            <NavLink
+                                                to="/user"
+                                                className="navLink"
+                                            >
+                                                Dashboard
+                                            </NavLink>
                                         </MenuItem>
                                         <MenuItem>
-                                            <NavLink to="/user/change-password" className='navLink'>ChangePassword</NavLink>
+                                            <NavLink
+                                                to="/user/change-password"
+                                                className="navLink"
+                                            >
+                                                ChangePassword
+                                            </NavLink>
                                         </MenuItem>
                                         <MenuItem>
-                                            <NavLink to='/' className='navLink' onClick={() => {
-                                                localStorage.removeItem('user')
-                                                localStorage.removeItem('admin')
-                                                props.authRemove()
-                                            }}>Logout</NavLink>
+                                            <NavLink
+                                                to="/"
+                                                className="navLink"
+                                                onClick={() => {
+                                                    localStorage.removeItem(
+                                                        "user"
+                                                    );
+                                                    localStorage.removeItem(
+                                                        "admin"
+                                                    );
+                                                    props.authRemove();
+                                                }}
+                                            >
+                                                Logout
+                                            </NavLink>
                                         </MenuItem>
                                     </Menu>
                                 </div>
-                            </span> :
-                            <NavLink to='/' className='navLink' onClick={() => {
-                                localStorage.removeItem('user')
-                                localStorage.removeItem('admin')
-                                props.authRemove()
-                            }}>Logout</NavLink>
-                        }
-                    </span> :
-                    <span>
-                        <NavLink to="/auth/register" className='navLink'>Signup</NavLink>
-                        |
-                        <NavLink to="/auth/login" className='navLink'>Login</NavLink>
+                            </span>
+                        ) : (
+                            <NavLink
+                                to="/"
+                                className="navLink"
+                                onClick={() => {
+                                    localStorage.removeItem("user");
+                                    localStorage.removeItem("admin");
+                                    props.authRemove();
+                                }}
+                            >
+                                Logout
+                            </NavLink>
+                        )}
                     </span>
-                }
+                ) : (
+                    <span>
+                        <NavLink to="/auth/register" className="navLink">
+                            Signup
+                        </NavLink>
+                        |
+                        <NavLink to="/auth/login" className="navLink">
+                            Login
+                        </NavLink>
+                    </span>
+                )}
             </span>
         </nav>
     );
 }
 
-
-const mapStateToProps = state => {
-    return { auth: state.auth }
-}
+const mapStateToProps = (state) => {
+    return { auth: state.auth };
+};
 
 export default connect(mapStateToProps, { authRemove })(Nav);
