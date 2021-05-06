956a83fa089ac593dcb6d56314fd26bece58d52c
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-05-06 18:01:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { postListFeched } from './app/postList'
-import { accountFetched } from './app/accounts'
-import { authAdd } from './app/auth'
-import Nav from './components/common/Nav';
+import { postListFeched } from "./app/postList";
+import { accountFetched } from "./app/accounts";
+import { authAdd } from "./app/auth";
+import Nav from "./components/common/Nav";
 
-import LandingPage from './components/pages/landingPage/LandingPage';
-import DetailsPage from './components/pages/detailsPage/DetailsPage';
-import AuthPage from './components/pages/authPage/AuthPage';
-import UserPage from './components/pages/userPage/UserPage';
-import AdminPage from './components/pages/adminPage/AdminPage';
-import NotFound from './components/common/NotFound';
-import './App.css';
-import AllPostList from './components/pages/landingPage/AllPostList';
-import EmailLoginPage from './components/pages/EmailLoginPage';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-
+import LandingPage from "./components/pages/landingPage/LandingPage";
+import DetailsPage from "./components/pages/detailsPage/DetailsPage";
+import AuthPage from "./components/pages/authPage/AuthPage";
+import UserPage from "./components/pages/userPage/UserPage";
+import AdminPage from "./components/pages/adminPage/AdminPage";
+import NotFound from "./components/common/NotFound";
+import "./App.css";
+import AllPostList from "./components/pages/landingPage/AllPostList";
+import EmailLoginPage from "./components/pages/EmailLoginPage";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App(props) {
+    useEffect(() => {
+        const getAllData = async () => {
+            props.accountFetched();
+            props.postListFeched();
+        };
 
-  useEffect(() => {
-    const getAllData = async () => {
-      props.accountFetched()
-      props.postListFeched()
-    };
+        const validateAuthID = async () => {
+            // Validating if token is valid
+            // await axios.post('/checkusertoken', token)
 
-    const validateAuthID = async () => {
-      // Validating if token is valid
-      // await axios.post('/checkusertoken', token)
+            props.authAdd(token, type);
+        };
 
-      props.authAdd(token, type)
-    }
+        // check if there's a user in local storage
+        const token =
+            localStorage.getItem("user") || localStorage.getItem("admin");
+        const type = localStorage.key(token);
 
-    // check if there's a user in local storage
-    const token = localStorage.getItem('user') || localStorage.getItem('admin')
-    const type = localStorage.key(token)
+        validateAuthID();
+        getAllData();
+    }, []);
 
-    validateAuthID()
-    getAllData();
-  }, []);
+    return (
+        <div className="App">
+            <Nav />
+            <div style={{ marginTop: "7vh" }}>
+                <Switch>
+                    <Route
+                        path="/login/email/:token"
+                        component={EmailLoginPage}
+                    />
+                    <Route path="/post-list" component={AllPostList} />
+                    <Route path="/details/:id" component={DetailsPage} />
+                    <Route path="/auth" component={AuthPage} />
+                    <Route path="/user" component={UserPage} />
+                    <Route path="/not-found" component={NotFound} />
+                    <Route path="/admin" component={AdminPage} />
+                    <Route path="/" exact component={LandingPage} />
+                    <Redirect to="/not-found" />
+                </Switch>
+            </div>
 
-
-  return (
-    <div className='App'>
-      <Nav />
-      <Switch>
-        <Route path='/login/email/:token' component={EmailLoginPage} />
-        <Route path='/post-list' component={AllPostList} />
-        <Route path='/details/:id' component={DetailsPage} />
-        <Route path='/auth' component={AuthPage} />
-        <Route path='/user' component={UserPage} />
-        <Route path='/not-found' component={NotFound} />
-        <Route path='/admin' component={AdminPage} />
-        <Route path='/' exact component={LandingPage} />
-        <Redirect to='/not-found' />
-      </Switch>
-   
-      <MessengerCustomerChat
-        pageId='114092927169420'
-        appId='697765701142285'
-      />
-
-    </div>
-  );
+            <MessengerCustomerChat
+                pageId="114092927169420"
+                appId="697765701142285"
+            />
+        </div>
+    );
 }
 
+const mapStateToProps = (state) => {
+    return { state };
+};
 
-const mapStateToProps = state => {
-  return { state }
-}
-
-export default connect(mapStateToProps,
-  { authAdd, postListFeched, accountFetched })(App);
+export default connect(mapStateToProps, {
+    authAdd,
+    postListFeched,
+    accountFetched,
+})(App);
